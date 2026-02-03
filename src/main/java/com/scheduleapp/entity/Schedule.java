bf218a1477b2b24d31fb3ff74b3a8d6706193bd8
puddingdream <edcrfv098@naver.com>
2026-02-03 20:34:42 +0900

    ✨ 스케줄 엔티티 만들기

---
+++ src/main/java/com/scheduleapp/entity/Schedule.java
@@ -0,0 +1,42 @@
+package com.scheduleapp.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "schedule")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long scheduleid;
+    private String title;
+    private String content;
+    private String writer;
+    private String password;
+
+
+    public Schedule(String title, String content, String writer, String password, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+        this.password = password;
+
+    }
+
+    public void update(String title, String content, String writer, String password, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+        this.password = password;
+
+    }
+
+
+}
